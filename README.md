1750245af68c6a2bcb147d3ffae5d282717fe0ae
yangfei <yangfei@boqii.net>
2015-11-18 10:27:24 +0800

    gulp-watch plugins

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 #gulp 实践-最终整合require 实现前端工程化
+##[官方教程](https://github.com/gulpjs/gulp)
 ##基本功能
 + ~~编译es6~~
 + ~~[使用compass](https://www.npmjs.com/package/gulp-compass)~~
-+ watch
++ ~~watch~~
 + 浏览器及时刷新,自动打开浏览器
 + 代码合并压缩 加文件指纹
 + 替换文件
 + 配合requirejs，r.js
-
--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,23 @@ const gulp = require('gulp');
 const path = require('path');
 const $ = require('gulp-load-plugins')();
 
+//配置路径
 var config = {
-  src:"./app",
+  sassfile:"app/scss/**/*.scss",
+  es6file:"app/es6/**/*.js",
   dist:"./dist"
 };
 
 gulp.task('es62es5', function(){
-    return gulp.src('app/es6/**/*.js')
+    return gulp.src(config.es6file)
         .pipe($.babel({
             presets: ['es2015']
         }))
         .pipe(gulp.dest('app/scripts/'));
 });
 
-
-
 gulp.task('compass', function() {
-  gulp.src('app/scss/**/*.scss')
+  gulp.src(config.sassfile)
     .pipe($.compass({
       project: path.join(__dirname, 'app'),
       css: 'css',
@@ -28,4 +28,10 @@ gulp.task('compass', function() {
     .pipe(gulp.dest('app/css'));
 });
 
-gulp.task("default",['es62es5']);
+//!!!!!!一定要注意－－－> watch 不能用 $.watch 而是用 gulp.watch
+gulp.task("watch",function(){
+  gulp.watch(config.sassfile,['compass']);
+  gulp.watch(config.es6file,['es62es5']);
+})
+
+gulp.task("default",['watch','compass','es62es5']);
